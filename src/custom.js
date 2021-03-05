8946f33be6b388928909399203f9039ad6282c36
unknown <vladimir.12.lazarevic@gmail.com>
2021-03-05 23:59:49 +0100

    I should add sidebar on bigger screen and menu bar dissapearing on scroll

--- src/custom.js
+++ src/custom.js
@@ -6,3 +6,29 @@ toggleMenu.onclick = function () {
     sidebar.classList.remove('show')
   } else sidebar.classList.add('show')
 }
+
+//scrolling
+
+function isVisible(element) {
+  let elementBox = element.getBoundingClientRect()
+  let distanceFromTop = -200
+
+  if (elementBox.top - window.innerHeight < distanceFromTop) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function scanDocument() {
+  let sectionList = document.querySelectorAll('.hidden')
+  sectionList.forEach((section) => {
+    if (isVisible(section)) {
+      section.classList.remove('hidden')
+    }
+  })
+}
+
+document.onscroll = function () {
+  scanDocument()
+}
