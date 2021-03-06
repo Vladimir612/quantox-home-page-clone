f1905bb7c4433788dd64c8ab56e559e491fc88ef
unknown <vladimir.12.lazarevic@gmail.com>
2021-03-06 21:25:24 +0100

    Finished clone but not in every detail

--- src/custom.js
+++ src/custom.js
@@ -32,3 +32,56 @@ function scanDocument() {
 document.onscroll = function () {
   scanDocument()
 }
+
+//changing color over different background
+
+const services = document.querySelector('.quantox-services')
+const fixed = document.querySelector('.sidebar.fixed')
+const translate = document.querySelector('.sidebar.fixed .translate ul')
+const socialMedia = document.querySelector('.sidebar.fixed .social-media')
+
+const links = document.querySelectorAll('.menu-link')
+
+window.addEventListener('scroll', function () {
+  links.forEach((link) => {
+    if (isOnTopOf(link, services)) {
+      link.classList.add('white')
+    } else link.classList.remove('white')
+
+    if (isOnTopOf(translate, services)) {
+      translate.classList.add('white')
+    } else translate.classList.remove('white')
+
+    if (isOnTopOf(socialMedia, services)) {
+      socialMedia.classList.add('white')
+    } else socialMedia.classList.remove('white')
+  })
+})
+
+function isOnTopOf(a, b) {
+  let fixedCoord = a.getBoundingClientRect()
+  let bgCoord = b.getBoundingClientRect()
+
+  if (
+    fixedCoord.top + fixedCoord.height - 10 >= bgCoord.top &&
+    fixedCoord.top + fixedCoord.height - 10 <= bgCoord.bottom
+  ) {
+    return true
+  }
+
+  return false
+}
+
+//scroll-to-next
+
+const mouse = document.querySelector('.mouse-scroll')
+
+const destinationSection = document.querySelector('body')
+
+function scrollToNextSection() {
+  destinationSection.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+    inline: 'nearest',
+  })
+}
